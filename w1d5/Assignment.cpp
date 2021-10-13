c9dce163147c01dc5fc628e4065c27fa7f9811d0
Caroline <caroline.palm@volvocars.com>
2021-10-13 15:14:08 +0200

    Assignments from week1 day 5

---
+++ w1d5/Assignment.cpp
@@ -0,0 +1,77 @@
+/* Implement an interface for shape, and derive circle, triangle, rectangle, square as children. Overload all three
+comparison operators to compare the shapes based on their area -- if equal then their perimeter should be
+considered. Only, and if only both area and perimeter are equal then the objects should be considered equal. */
+
+#include <iostream>
+#include "shapes.h"
+
+// Shape is smaller either if the area is smaller OR if the area is equal and the perimiter is smaller
+// What is the difference between using this and s.?
+bool Shape::operator<(const Shape &s){
+    bool returnVal = false;
+    if ((this->Area < s.Area) || ((this->Area == s.Area)&&(this->Perimeter < s.Perimeter))){
+        returnVal = true;
+    }
+    return returnVal;
+}
+
+//Shape is larger either if the area is larger OR if the area is equal and the perimiter is larger
+bool Shape::operator>(const Shape &s){
+    bool returnVal = false;
+    if ((this->Area > s.Area) || ((this->Area == s.Area)&&(this->Perimeter > s.Perimeter))){
+        returnVal = true;
+    }
+    return returnVal;
+}
+
+//Shape is equal if both area and perimeter is equal
+bool Shape::operator==(const Shape &s){
+    bool returnVal = false;
+    if ((this->Area == s.Area) && (this->Perimeter == s.Perimeter)){
+        returnVal = true;
+    }
+    return returnVal;
+}
+
+
+Shape::~Shape(){} 
+
+int main(){
+
+    Shape *circ1 = new Circle(5);
+    Shape *circ2 = new Circle(100);
+    Shape *tri1 = new Triangle(10,20);
+    Shape *tri2 = new Triangle(2,5);
+    Shape *rec1 = new Rectangle(3,4);
+    Shape *rec2 = new Rectangle(30,60);
+    Shape *squ1 = new Square(10);
+    Shape *squ2 = new Square(100);
+
+
+    Shape *shapes[8] = {circ1, circ2, tri1, tri2, rec1, rec2, squ1, squ2};
+    std::string nameShapes[8] = {"circ1", "circ2", "tri1", "tri2", "rec1", "rec2", "squ1", "squ2"};
+
+    for(size_t i=0; i<8; i++){
+        std::cout << nameShapes[i] << ": \t";
+        shapes[i] ->print();
+    }
+
+    for(size_t i=0; i<8; i++){
+        for(size_t j=i+1; j<8; j++){
+            if (*shapes[i] < *shapes[j]){
+                std::cout << nameShapes[i] << " is smaller than " << nameShapes[j] << std::endl;                
+            }
+            else if (*shapes[i]>*shapes[j]){
+                std::cout << nameShapes[i] << " is larger than " << nameShapes[j] << std::endl;
+            }
+            else if (*shapes[i]==*shapes[j]){
+                std::cout << nameShapes[i] << " is equal to " << nameShapes[j] << std::endl;
+            }
+        }
+    }
+
+    
+    return 0;   
+
+
+}
